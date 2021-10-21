2c8f634a5b3953fdb4c1bdfea1cbbcf980003f91
Douglas Lovera <doulovera@gmail.com>
2021-10-20 19:38:39 -0500

    Add and Configure CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
@@ -8,6 +9,18 @@ const port = process.env.PORT || 3000;
 
 // Este middleware nativo es necesario para enviar el body del POST
 app.use(express.json())
+// CORS
+const ALLOW_LIST = ['http://localhost:3000/', 'http://localhost:8080/'];
+const CORS_OPTIONS = {
+  origin: (origin, callback) => {
+    if (ALLOW_LIST.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acceso no Permitido'), false)
+    }
+  }
+}
+app.use(cors(CORS_OPTIONS))
 
 app.get('/', (req, res) => {
   res.send('wuju asd')
